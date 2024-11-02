21e8e540dd9b26a2ca2ca623e3e2358d306075e5
Yung Lord <marikoryagina.den@yandex.ru>
2024-11-02 11:27:23 +0300

    Первый коммит

---
+++ Tracker/TrackersViewController/TrackerCategoryViewController.swift
@@ -0,0 +1,131 @@
+import UIKit
+
+//MARK: - Tracker Category View Controller
+
+final class TrackerCategoryViewController: UIViewController {
+    
+    //MARK: - Public Properties
+    
+    let viewModel = TrackerCategoryViewModel()
+    var categorySelectionHandler: ((TrackerCategory) -> Void)?
+    
+    //MARK: - Private Properties
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Категория"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        return label
+    }()
+    
+    private lazy var errorImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "ErrorStar"))
+        return imageView
+    }()
+    
+    private lazy var addCategoryButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Добавить категорию", for: .normal)
+        button.layer.cornerRadius = 16
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.backgroundColor = .ypBlack
+        button.setTitleColor(.ypWhite, for: .normal)
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        button.addTarget(
+            self,
+            action: #selector(addCategoryButtonTapped),
+            for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .ypWhite
+        tableView.register(TrackerCategoryCell.self, forCellReuseIdentifier: TrackerCategoryCell.identifier)
+        tableView.separatorStyle = .singleLine
+        tableView.separatorColor = UIColor.ypGray
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        tableView.rowHeight = 75
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .ypWhite
+        setupUI()
+        setupConstraints()
+        bindViewModel()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupUI() {
+        [titleLabel, errorImageView, addCategoryButton, tableView].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 38),
+            
+            errorImageView.widthAnchor.constraint(equalToConstant: 80),
+            errorImageView.heightAnchor.constraint(equalToConstant: 80),
+            errorImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            errorImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            addCategoryButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            addCategoryButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            addCategoryButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            
+            tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 38),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: addCategoryButton.topAnchor, constant: -20)
+        ])
+    }
+    
+    private func bindViewModel() {
+        viewModel.updateUI = { [weak self] in
+            self?.updateUI()
+        }
+        viewModel.saveCategory = { [weak self] in
+            self?.tableView.reloadData()
+        }
+        updateUI()
+    }
+    
+    private func updateUI() {
+        if viewModel.categories.isEmpty {
+            errorImageView.isHidden = false
+            tableView.isHidden = true
+        } else {
+            errorImageView.isHidden = true
+            tableView.isHidden = false
+        }
+        tableView.reloadData()
+    }
+    
+    
+    //MARK: - Action
+    
+    @objc private func addCategoryButtonTapped() {
+        if let selectedCategory = viewModel.selectedCategory {
+            categorySelectionHandler?(selectedCategory)
+            dismiss(animated: true, completion: nil)
+        } else {
+            let newCategoryVC = TrackerNewCategoryVC()
+            newCategoryVC.onCategorySave = { [weak self] categoryName in
+                self?.viewModel.addCategory(with: categoryName.title)
+            }
+            present(newCategoryVC, animated: true, completion: nil)
+        }
+    }
+}
---
+++ Tracker/TrackersViewController/TrackerTypeSelectionVC.swift
@@ -0,0 +1,129 @@
+import UIKit
+
+// MARK: - TrackerTypeSelectionDelegate
+
+protocol TrackerTypeSelectionDelegate: AnyObject {
+    func didSelectTrackerType(_ type: TrackerType)
+}
+
+// MARK: - Enum
+
+enum TrackerType {
+    case habit
+    case irregularEvent
+}
+
+// MARK: - TrackerTypeSelection
+
+final class TrackerTypeSelectionViewController: UIViewController {
+    
+    // MARK: - Public Properties
+    
+    weak var delegate: TrackerTypeSelectionDelegate?
+    
+    // MARK: - Private Properties
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Создание трекера"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        return label
+    }()
+    
+    private lazy var habitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle ("Привычка", for: .normal)
+        button.setTitleColor(.ypWhite, for: .normal)
+        button.backgroundColor = .ypBlack
+        button.layer.cornerRadius = 16
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        button.addTarget(
+            self,
+            action: #selector(habitButtonTapped),
+            for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var irregularEventButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Нерегулярные событие", for: .normal)
+        button.setTitleColor(.ypWhite, for: .normal)
+        button.backgroundColor = .ypBlack
+        button.layer.cornerRadius = 16
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        button.addTarget(
+            self,
+            action: #selector(irregularEventButtonTapped),
+            for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .ypWhite
+        setupUI()
+        setupConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    
+    private func setupUI() {
+        [titleLabel, habitButton, irregularEventButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 38),
+            
+            habitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            habitButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            habitButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            habitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            irregularEventButton.topAnchor.constraint(equalTo: habitButton.bottomAnchor, constant: 16),
+            irregularEventButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            irregularEventButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            irregularEventButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    private func setupAppearance() {
+        let isDarkMode = traitCollection.userInterfaceStyle == .dark
+        view.backgroundColor = isDarkMode ? .ypBlack : .ypWhite
+        titleLabel.textColor = isDarkMode ? .ypWhite : .ypBlack
+        habitButton.backgroundColor = isDarkMode ? .ypWhite : .ypBlack
+        habitButton.setTitleColor(isDarkMode ? .ypBlack : .ypWhite, for: .normal)
+        irregularEventButton.backgroundColor = isDarkMode ? .ypWhite : .ypBlack
+        irregularEventButton.setTitleColor(isDarkMode ? .ypBlack : .ypWhite, for: .normal)
+    }
+    
+    // MARK: - Private Methods
+    
+    private func showCreateTrackerScreen(with type: TrackerType) {
+        let createTrackerVC = TrackerCreationViewController()
+        createTrackerVC.trackerType = type
+        createTrackerVC.modalPresentationStyle = .fullScreen
+        present(createTrackerVC, animated: true, completion: nil)
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func habitButtonTapped() {
+        delegate?.didSelectTrackerType(.habit)
+        showCreateTrackerScreen(with: .habit)
+    }
+    
+    @objc private func irregularEventButtonTapped() {
+        delegate?.didSelectTrackerType(.irregularEvent)
+        showCreateTrackerScreen(with: .irregularEvent)
+    }
+}
